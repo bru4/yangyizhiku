b86f79e54730252c73b6acf31d438a3f74c5f447
bruce <shiyj@5mutian.com>
2017-06-22 17:25:46 +0800

    修改注销按钮

--- src/views/components/logoutButton.vue
+++ src/views/components/logoutButton.vue
@@ -1,5 +1,18 @@
 <template>
-  <el-button type="text" @click="logout">注销</el-button>
+  <div class="">
+    <el-button type="text" @click="logout">注销</el-button>
+    <el-dialog
+      title="提示"
+      :visible.sync="logoutDialogVisible"
+      size="tiny"
+      :before-close="handleClose">
+      <span>您确定注销？</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="logoutDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="logoutOk">确 定</el-button>
+      </span>
+    </el-dialog>
+  </div>
 </template>
 <script>
 import { mapState } from 'vuex'
@@ -9,13 +22,17 @@ export default {
   name: "Button",
   data: function data() {
     return {
+      logoutDialogVisible: false
     }
   },
   computed: mapState([
     'token'
   ]),
   methods: {
-    logout: function(){
+    logout: function() {
+      this.logoutDialogVisible = true
+    },
+    logoutOk: function(){
       this.$store.commit(types.LOGOUT)
       this.$router.push({
         path: '/login'
